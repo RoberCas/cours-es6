c615f5e09f9b52d9666ba7bad15670e6822cbb83
RoberCas <robertoderellan@gmail.com>
2016-12-06 14:39:25 +0100

    upt step6

--- dev/app/app.js
+++ dev/app/app.js
@@ -20,13 +20,7 @@ class HomePage {
   }
 
   initUI(){
-<<<<<<< HEAD
-    // remove all section before display UI
-    if(document.getElementsByTagName("section")[0]){
-      document.getElementsByTagName("section")[0].parentNode.removeChild(document.getElementsByTagName("section")[0])
-    }
-=======
->>>>>>> upstream/step2
+
     // create page skeleton
     let pageSkeleton = `
       <section>
@@ -44,61 +38,7 @@ class HomePage {
     this.loadEventUI()
 
   }
-<<<<<<< HEAD
 
-  loadEventUI(){
-    let loginForm = document.getElementsByTagName("form")[0];
-    loginForm.addEventListener("submit",  event => this.onLogin(event), false)
-
-
-  }
-
-  onLogin(event){
-    event.preventDefault()
-    let validationInput = 0
-    let formInput = {}
-    let form = document.forms[0].elements
-    for (let i = 0; i < form.length; i++) {
-      if(form[i].value){
-        formInput[form[i].name] = form[i].value
-        validationInput++
-      }
-    }
-    console.log(formInput)
-    if(validationInput === 2){
-      console.log('load UserPage')
-      new UserPage(this.appBody,formInput);
-    }
-  }
-
-}
-
-class UserPage {
-
-  constructor(appBody,formInput){
-    this.appBody = appBody
-    this.formData = formInput
-    this.pageTitle = "Hello";
-    this.initUI();
-  }
-
-    initUI(){
-      // remove all section before display UI
-      if(document.getElementsByTagName("section")[0]){
-        document.getElementsByTagName("section")[0].parentNode.removeChild(document.getElementsByTagName("section")[0])
-      }
-
-      // create page skeleton
-      let pageSkeleton = `
-        <section>
-          <h1>${this.pageTitle} ${this.formData.email} !</h1>
-        </section>
-      `;
-      // add page skeleton in body
-      this.appBody.insertAdjacentHTML( 'afterbegin', pageSkeleton )
-    }
-=======
->>>>>>> upstream/step2
 }
 let myApp = new MyApp();
 myApp.start();
