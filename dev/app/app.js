ab2c8bde355e45a0536c188a3810718f3f7ffc79
RoberCas <robertoderellan@gmail.com>
2016-12-07 09:08:57 +0100

    update step6 c

--- dev/app/app.js
+++ dev/app/app.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 /**
 * @Author: Nicolas Fazio <webmaster-fazio>
 * @Date:   01-09-2016
@@ -10,7 +8,6 @@
 
 import { HomePage } from './pages/home/home';
 
->>>>>>> upstream/step6
 class MyApp {
 
   constructor(){
@@ -24,37 +21,5 @@ class MyApp {
 
 }
 
-<<<<<<< HEAD
-class HomePage {
-
-  constructor(appBody){
-    this.appBody = appBody
-    this.pageTitle = 'Hello world!';
-    this.initUI();
-  }
-
-  initUI(){
-
-    // create page skeleton
-    let pageSkeleton = `
-      <section>
-        <h1>${this.pageTitle}</h1>
-        <form>
-          <p>
-            <label for="email">Email:</label> <input type="email" name="email" value="" placeholder="votreemail.ch"  /><br/>
-            <label for="password">Password:</label> <input type="password" name="password" value=""  /><br/>
-            <button>Login</button>
-          </p>
-        </form>
-      </section>`;
-    // add page skeleton in body
-    this.appBody.insertAdjacentHTML( 'afterbegin', pageSkeleton )
-    this.loadEventUI()
-
-  }
-
-}
-=======
->>>>>>> upstream/step6
 let myApp = new MyApp();
 myApp.start();
